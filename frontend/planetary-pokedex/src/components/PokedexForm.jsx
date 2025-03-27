9fdec19dedc632c035cc2fffbb1526590567c57e
Mangal-Devanshu <sarojmangal7600@gmail.com>
2025-03-27 23:20:58 +0530

    Done

--- frontend/planetary-pokedex/src/components/PokedexForm.jsx
+++ frontend/planetary-pokedex/src/components/PokedexForm.jsx
@@ -139,7 +139,7 @@ const PokedexForm = () => {
     try {
       setIsSearching(true); // Start searching animation
       
-      const response = await axios.post("https://planetary-pokedex.onrender.com/analyze", formData, {
+      const response = await axios.post("http://127.0.0.1:5000//analyze", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
